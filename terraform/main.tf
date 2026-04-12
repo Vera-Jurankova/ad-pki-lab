d79662289ed4aba2a0a7bc155b72660a9b39ffc3
Vera-Jurankova <VJurankova@proton.me>
2026-04-12 15:38:29 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
 resource "local_file" "test" {
   content  = "Hello from Terraform via GitHub Actions"
   filename = "C:\\temp\\terraform-test.txt"
